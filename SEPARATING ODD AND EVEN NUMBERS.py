e991cb82ae23f389f32cdf57c61555d2e2bea62d
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-26 11:09:19 +0800

    creating two files separating the odd and even numbers

--- SEPARATING ODD AND EVEN NUMBERS.py
+++ SEPARATING ODD AND EVEN NUMBERS.py
@@ -1,2 +1,7 @@
 #opening the numbers file
 my_file = open("numbers.txt","r")
+
+#creating two files separating the odd and even numbers
+numbers = [int(line.strip()) for line in my_file.readlines()]
+even_numbers = [num for num in numbers if num % 2 == 0]
+odd_numbers = [num for num in numbers if num % 2 != 0]
