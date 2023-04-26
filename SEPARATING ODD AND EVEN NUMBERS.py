dec8f999c7864a983579daccfc513c8c7b46a8c6
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-26 11:13:55 +0800

    write odd numbers to odd.txt

--- SEPARATING ODD AND EVEN NUMBERS.py
+++ SEPARATING ODD AND EVEN NUMBERS.py
@@ -5,3 +5,8 @@ my_file = open("numbers.txt","r")
 numbers = [int(line.strip()) for line in my_file.readlines()]
 even_numbers = [num for num in numbers if num % 2 == 0]
 odd_numbers = [num for num in numbers if num % 2 != 0]
+
+#write odd numbers to odd.txt
+with open("odd.txt", "w") as odd_file:
+    for num in odd_numbers:
+        odd_file.write(str(num) + "\n")
